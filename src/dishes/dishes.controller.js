cb1e46d5688cd2a1517f718079130d490a2ea74b
Avery King <avery.n.king@wmich.edu>
2022-03-16 18:36:53 -0400

    ``findDish`` middleware added

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -6,28 +6,31 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 // Use this function to assign ID's when necessary
 const nextId = require("../utils/nextId");
 
-const list = (req,res) => {
-    res.json({data:dishes})
+const list = (req, res) => {
+    res.json({data: dishes})
 }
 
-const read = (req,res,next) => {
+const findDish = (req, res, next) => {
     const foundDish = dishes.find(dish => dish.id === req.params.dishId);
-    if(!foundDish) {
+    if (!foundDish) {
         return next({
-            status:404,
-            message:"dish not found"
+            status: 404,
+            message: "dish not found"
         })
-
-    } else {
-        return res.json({data:foundDish});
     }
+    res.locals.foundDish = foundDish
+    next();
 }
 
+const read = (req, res, next) => {
+    return res.json({data: res.locals.foundDish});
+
+}
 
 function validateParam(propertyName) {
     return function (req, res, next) {
         const {data = {}} = req.body;
-        if(data.price < 0) {
+        if (data.price < 0) {
             return next({status: 400, message: `price must be greater than 0`});
         }
         if (data[propertyName]) {
@@ -38,15 +41,18 @@ function validateParam(propertyName) {
 
 }
 
-const create = (req,res,next) => {
-        req.body.data.id = dishes.length + 1
-        dishes.push(req.body.data)
-        res.status(201).json({data: req.body.data})
+const create = (req, res, next) => {
+    req.body.data.id = dishes.length + 1
+    dishes.push(req.body.data)
+    res.status(201).json({data: req.body.data})
 
 }
 
 module.exports = {
-    list, read,
+    list, read: [
+        findDish,
+        read
+    ],
     create: [
         validateParam("name"),
         validateParam("description"),
