aa2eaf7f6de3e814acfb81313a294996fa82f49b
Avery King <avery.n.king@wmich.edu>
2022-03-16 19:22:59 -0400

    ``list`` method for orders implemented

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -6,4 +6,11 @@ const orders = require(path.resolve("src/data/orders-data"));
 // Use this function to assigh ID's when necessary
 const nextId = require("../utils/nextId");
 
+const list = (req,res) => {
+    res.json({data:orders});
+}
 // TODO: Implement the /orders handlers needed to make the tests pass
+
+module.exports = {
+    list
+}
