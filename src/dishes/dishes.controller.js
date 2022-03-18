2cc948ea69a41b8f81bdb7b15ef25d9303e91285
Avery King <avery.n.king@wmich.edu>
2022-03-17 20:41:53 -0400

    anonymous function named; comments added

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -2,25 +2,31 @@ const path = require("path");
 const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
+// finds a dish
 const findDish = (req, res, next) => {
     const foundDish = dishes.find(dish => dish.id === req.params.dishId);
     if (!foundDish) {
+        // dish not found
         return next({
             status: 404,
             message: "dish not found"
         })
     }
+    // save dish in res.locals
     res.locals.foundDish = foundDish
     next();
 }
 
+// validate request parameters
 function validateParam(propertyName) {
     return function (req, res, next) {
         const {data = {}} = req.body;
         if (data.price < 0 || typeof (data.price) !== 'number') {
+            // validate price > 0
             return next({status: 400, message: `price must be greater than 0`});
         }
         if (data[propertyName]) {
+            // continue to next if property present
             return next();
         }
         next({status: 400, message: `Must include a ${propertyName}`});
@@ -28,16 +34,26 @@ function validateParam(propertyName) {
 
 }
 
+// list all dishes
 const list = (req, res) => res.json({data: dishes});
+// read single dish
 const read = (req, res) => res.json({data: res.locals.foundDish});
+// delete a dish
 const deleteDish = (req, res) => res.status(405).json({error: 'not implemented'});
 
+// create a dish
 const create = (req, res) => {
     req.body.data.id = nextId();
     dishes.push(req.body.data);
-    res.status(201).json({data: req.body.data})
+    res.status(201).json({data: req.body.data});
 }
 
+/**
+ *
+ * updates a dish
+ * dish id is ``not`` able to be overwritten
+ *
+ */
 const update = (req, res) => {
     if (req.body.data.id && req.body.data.id !== req.params.dishId) {
         return res.status(400).json({error: `id ${req.body.data.id} does not equal ${req.params.dishId}`})
@@ -50,6 +66,7 @@ const update = (req, res) => {
     res.locals.foundDish = req.body.data;
     res.json({data: res.locals.foundDish})
 }
+
 
 module.exports = {
     create: [
--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -3,29 +3,38 @@ const orders = require(path.resolve("src/data/orders-data"));
 
 const nextId = require("../utils/nextId");
 
+// Validate request parameters
 function validateParam(propertyName) {
-    return function (req, res, next) {
+    return function isParamValid (req, res, next)  {
         const {data = {}} = req.body;
         if(propertyName === 'status' && data['status'] === 'invalid') {
+            // give 400 error if status is invalid
             next({status: 400, message: `status is invalid`});
         }
         if (data[propertyName]) {
+            // all good, go to next
             return next();
         }
+        // give 400 error if required property missing
         next({status: 400, message: `Must include a ${propertyName}`});
     };
 }
 
+// Validate dishes array
 const validateDishesArray = (req,res,next) => {
+    //cannot be empty
     if (req.body.data.dishes.length === 0) {
         res.status(400).json({error: 'dishes array cannot be empty'})
     }
+    // must be array
     if (typeof (req.body.data.dishes) !== 'object') {
         res.status(400).json({error: 'dishes must be an array'})
     }
+    // dish must have positive quantity
     if (req.body.data.dishes.some(x => !x.quantity)) {
         res.status(400).json({error: 'dish must have a quantity of at least 1 (cannot be 0!)'});
     }
+    // dish quantity must be an integer
     req.body.data.dishes.forEach(dish => {
         if (!Number.isInteger(dish.quantity)) {
             res.status(400).json({error: `quantity must be an integer (1, 2, 3, 4, 5, etc... )`})
@@ -34,10 +43,12 @@ const validateDishesArray = (req,res,next) => {
     next();
 }
 
+// validate that order exists
 const doesOrderExist = (req,res, next) => {
     const order = orders.find(x => x.id === req.params.orderId);
     if (!order) {
-        next({
+        // return 404 error if not found
+      return next({
             status:404,
             message: `order ${req.params.orderId} not found`
         })
@@ -46,29 +57,36 @@ const doesOrderExist = (req,res, next) => {
     next();
 }
 
+// list all orders
 const list = (req, res) => res.json({data: orders});
 
+// find individual
 const read = (req, res) => res.json({data: res.locals.order});
 
+// create new order
 const create = (req, res) => {
     req.body.data.id = nextId();
     orders.push(req.body.data);
     res.status(201).json({data: req.body.data});
 }
 
+
+// update order
 const update = (req, res) => {
     if (req.body.data.id && req.body.data.id !== req.params.orderId) {
-        return res.status(400).json({error: `id ${req.body.data.id} does not equal ${req.params.orderId}`})
+        return res.status(400).json({error: `id ${req.body.data.id} does not equal ${req.params.orderId}`});
     }
     if (!req.body.data.id) {
         res.locals.order = req.body.data;
+        // ** does not allow id to be overwritten
         res.locals.order.id = req.params.orderId;
-        res.json({data: res.locals.order})
+        res.json({data: res.locals.order});
     }
     res.locals.order = req.body.data;
     res.json({data: res.locals.order});
 }
 
+// delete order
 const deleteOrder = (req,res) => {
     if(res.locals.order.status !== 'pending') {
         res.status(400).json({error:'order can only be deleted if status is pending'})
